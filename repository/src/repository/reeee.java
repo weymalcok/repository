d2229f8b73007496d84ca6b6327506102cfb8087
aidata <aidata@10.121.3.187>
2022-10-27 10:43:25 +0300

    message

--- repository/src/repository/reeee.java
+++ repository/src/repository/reeee.java
@@ -7,13 +7,17 @@ import java.util.Scanner;
 public class reeee {
 
 	public static void main(String[] args) {
-		int[] array = new int[10];
-		
+		final int ARRAY_LENGTH = 10;
+		int[] array = new int[ARRAY_LENGTH];
+		for(int counter = 0; counter < array.length; counter++) {
+			
+			array[counter] = 2 + 2 * counter;			
+		}
+				
 		System.out.printf("%s%8%n", "Index", "Value");
 		
 		for(int counter = 0; counter < array.length; counter++)  {
 				System.out.printf("%5d%8d%n", counter, array[counter]);
 	}
-
 }
 }

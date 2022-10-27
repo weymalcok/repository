2147b7672a0e6a2680f321abbf7231e92dd3e047
aidata <aidata@10.121.3.187>
2022-10-27 10:11:10 +0300

    message

--- repository/src/repository/reeee.java
+++ repository/src/repository/reeee.java
@@ -1,10 +1,19 @@
 package repository;
 
+import java.lang.reflect.Array;
+
+import java.util.Scanner;
+
 public class reeee {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int[] array = new int[10];
+		
+		System.out.printf("%s%8%n", "Index", "Value");
+		
+		for(int counter = 0; counter < array.length; counter++)  {
+				System.out.printf("%5d%8d%n", counter, array[counter]);
 	}
 
+}
 }
